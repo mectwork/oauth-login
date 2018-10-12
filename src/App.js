558319466db6f160ebc5eb033e46c439d9ec41eb
Manuel Emilio Carpio <mectwork@gmail.com>
2018-10-12 14:55:09 -0400

    Adding GoogleLogin

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import GoogleLogin from 'react-google-login';
+
 import './App.css';
 
 class App extends Component {
+
   render() {
+    const responseGoogle = (response) => {
+      console.log(response);
+    }
+    console.log(process.env.REACT_APP_GOOGLE_CLIENT_ID);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <GoogleLogin
+          clientId={`${process.env.REACT_APP_GOOGLE_CLIENT_ID}`}
+          buttonText="Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+        />
       </div>
+
     );
   }
 }
